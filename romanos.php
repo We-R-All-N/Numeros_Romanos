e631b43a8354b79817e2efea12aeaaec792fabb9
Francisco Gradndin <franciscograndin@hotmail.com>
2017-09-09 18:13:47 -0300

    Separa numero en digitos

--- romanos.php
+++ romanos.php
@@ -1,4 +1,19 @@
 <?php
+    print "Programa para pasar números a notación romana\n Ingrese un números\n";
 
+    $numero = 4000;
 
+   	if($numero > 1000)
+   	{
+    $m=floor($numero/1000);
+   	}
+   	if($numero > 100)
+   	{
+    $c=floor($numero/100);
+   	}
+   	if($numero > 10)
+   	{
+    $d=floor($numero/10);
+   	}
+   	
 ?>
