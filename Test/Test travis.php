6fc443e36337579a64dd2b0fa5818e8b6803742a
Nismansomostodos <franciscograndin@hotmail.com>
2017-09-19 16:40:05 -0300

    Elimina test que no sirve

--- Test/Test travis.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Ejemplo;
-use PHPUnit\Framework\TestCase;
-class NumeroromanoTest extends TestCase {
-  public function test() {
-    $numero = 3900;
-    $this->assertEquals($unidades, MMMCM);
-  }
-}
